770643712fa62ad1d8bb849104361b16e677a97c
Geoffroy Muller <johndoe@example.comgeoffroym2@gmail.com>
2019-02-28 11:26:46 +0100

    index.php vue

--- Projet_1/PHP/index.php
+++ Projet_1/PHP/index.php
@@ -15,6 +15,25 @@ $db->addConnection(parse_ini_file('src/conf/conf.ini'));
 $db->setAsGlobal();
 $db->bootEloquent();
 
-
-
+echo '</br>========================</br>';
+echo 'lister les jeux dont le nom contient \'Mario\' </br>';
+echo 'Requete : Select ...</br>';
+echo '<a href="">Cliquer ici pour tester la requete</a>';
+echo '</br>========================</br>';
+echo 'lister les compagnies installées au Japon</br>';
+echo 'Requete : Select ...</br>';
+echo '<a href="">Cliquer ici pour tester la requete</a>';
+echo '</br>========================</br>';
+echo 'lister les plateformes dont la base installée est >= 10 000 000 </br>';
+echo 'Requete : Select ...</br>';
+echo '<a href="">Cliquer ici pour tester la requete</a>';
+echo '</br>========================</br>';
+echo 'lister 442 jeux à partir du 21173ème </br>';
+echo 'Requete : Select ...</br>';
+echo '<a href="">Cliquer ici pour tester la requete</a>';
+echo '</br>========================</br>';
+echo 'lister les jeux, afficher leur nom et deck, en paginant (taille des pages : 500) </br>';
+echo 'Requete : Select ...</br>';
+echo '<a href="">Cliquer ici pour tester la requete</a>';
+echo '</br>========================</br>';
 
