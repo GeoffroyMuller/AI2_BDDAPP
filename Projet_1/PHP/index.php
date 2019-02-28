53ac6b926636b4a5fc11e39e41f6dba5ba2a32e5
Adrien <adrien.palmieri9@etu.univ-lorraine.fr>
2019-02-28 14:39:13 +0100

    ajout question 3

--- Projet_1/PHP/index.php
+++ Projet_1/PHP/index.php
@@ -31,3 +31,10 @@ $companies = Company::where('location_country','=', 'Japan')->get();
 foreach($companies as $company) {
     echo "Nom de la compagnie : $company->name <br>";
 }
+
+//Question 3 : plateformes ayant plus de 10M d'installations
+echo "<h3>Question n°3 : liste des plateformes ayant plus de 10M d'installations</h3>";
+$platforms = Platform::where('install_base', '>=','10000000')->get();
+foreach($platforms as $platform) {
+    echo "Nom de la plateforme : $platform->name <br>";
+}
