3946dd2c4a7f546aa4b60f0fe418a9964a81bf6f
Adrien <adrien.palmieri9@etu.univ-lorraine.fr>
2019-02-28 10:50:15 +0100

    modification index

--- Projet_1/index.php
+++ Projet_1/index.php
@@ -1,8 +1,16 @@
 <?php
-require_once 'vendor/autoload.php';
-use Illuminate\Database\Capsule\Manager as DB;
+/**
+ * Created by PhpStorm.
+ * Date: 2019-02-28
+ * Time: 10:24
+ */
 
-$db = new DB();
-$db->addConnection(parse_ini_file('./src/conf/conf.ini'));
+require_once 'vendor/autoload.php';
+
+
+use Illuminate\Database\Capsule\Manager as Manager;
+
+$db = new Manager();
+$db->addConnection(parse_ini_file('src/conf/conf.ini'));
 $db->setAsGlobal();
 $db->bootEloquent();
