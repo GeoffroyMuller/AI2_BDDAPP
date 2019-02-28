cfacaa90d043f6c4f6c1d2ee37a898f3b36a01a5
Geoffroy Muller <johndoe@example.comgeoffroym2@gmail.com>
2019-02-28 10:48:20 +0100

    base php

---
+++ Projet_1/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'vendor/autoload.php';
+use Illuminate\Database\Capsule\Manager as DB;
+
+$db = new DB();
+$db->addConnection(parse_ini_file('./src/conf/conf.ini'));
+$db->setAsGlobal();
+$db->bootEloquent();
