ff19ae9a1ef057435ccea7bdaa8ad52489da1110
Adrien <adrien.palmieri9@etu.univ-lorraine.fr>
2019-02-28 14:26:49 +0100

    ajout question 2

--- Projet_1/PHP/index.php
+++ Projet_1/PHP/index.php
@@ -7,7 +7,9 @@
 
 require_once 'vendor/autoload.php';
 
-
+use aibdd\models\Game as Game;
+use aibdd\models\Platform as Platform;
+use aibdd\models\Company as Company;
 use Illuminate\Database\Capsule\Manager as Manager;
 
 $db = new Manager();
@@ -21,4 +23,11 @@ echo "<h3>Question n°1 : liste des jeux contenant Mario dans leur titre</h3>";
 $games = Game::where('name', 'LIKE','%mario%')->get();
 foreach($games as $game) {
     echo "Nom du jeu : $game->name <br>";
+}
+
+//Question 2 : toutes les entreprises installees au Japon
+echo "<h3>Question n°2 : liste des compagnies installees au japon</h3>";
+$companies = Company::where('location_country','=', 'Japan')->get();
+foreach($companies as $company) {
+    echo "Nom de la compagnie : $company->name <br>";
 }
