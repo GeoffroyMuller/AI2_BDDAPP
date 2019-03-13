8791ed66753e77bef4d15f102342c48c18a372fe
Adrien <adrien.palmieri9@etu.univ-lorraine.fr>
2019-03-13 21:34:32 +0100

    correction model company

--- Projet_1/PHP/src/models/Company.php
+++ Projet_1/PHP/src/models/Company.php
@@ -8,11 +8,11 @@ final class Company extends \Illuminate\Database\Eloquent\Model{
     public $timestamps = false ;
 
     public function games(){
-        return $this->belongsToMany("gamepedia\models\Game", "game_developers", "com_id", "game_id");
+        return $this->belongsToMany("gamepedia\models\Game", "game_developers", "comp_id", "game_id");
     }
 
     public function jeuxPublies(){
-        return $this->belongsToMany("gamepedia\models\Game", "game_publishers", "com_id", "game_id");
+        return $this->belongsToMany("gamepedia\models\Game", "game_publishers", "comp_id", "game_id");
     }
 
 }
