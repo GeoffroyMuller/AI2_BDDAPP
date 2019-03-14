23556591bdf90b5c9482152fa70f14294911ca7a
Adrien <adrien.palmieri9@etu.univ-lorraine.fr>
2019-03-14 10:18:17 +0100

    ajout vue oubliee

--- Projet_1/PHP/src/vues/VuePrincipal.php
+++ Projet_1/PHP/src/vues/VuePrincipal.php
@@ -7,119 +7,68 @@ use Slim\Slim as Slim;
 class VuePrincipal
 {
     private $elements;
-    private $selecteur;
     private $app;
     private $urlServeur;
-    public function __construct($tabAffichage, $selecteur)
+    public function __construct($tabAffichage)
     {
         $this->elements = $tabAffichage;
-        $this->selecteur = $selecteur;
         $this->app = \Slim\Slim::getInstance();
         $this->urlServeur =  Slim::getInstance()->request()->getRootUri();
     }
 
     public function htmlquestion()
     {
-        $urlQuestion1 = Slim::getInstance()->urlFor("PROJET1",['id' => 1]);
-        $urlQuestion2 = Slim::getInstance()->urlFor("PROJET1",['id' => 2]);
-        $urlQuestion3 = Slim::getInstance()->urlFor("PROJET1",['id' => 3]);
-        $urlQuestion4 = Slim::getInstance()->urlFor("PROJET1",['id' => 4]);
-        $urlQuestion5 = Slim::getInstance()->urlFor("Projet1_Q5");
+        $app = Slim::getInstance();
+        $urlProjet1Question1 = $app->urlFor("PROJET1",['id' => 1]);
+        $urlProjet1Question2 = $app->urlFor("PROJET1",['id' => 2]);
+        $urlProjet1Question3 =$app->urlFor("PROJET1",['id' => 3]);
+        $urlProjet1Question4 = $app->urlFor("PROJET1",['id' => 4]);
+        $urlProjet1Question5 = $app->urlFor("Projet1_Q5");
+
+        $urlProjet2Question1 = $app->urlFor("PROJET2",['id' => 1]);
+        $urlProjet2Question2 = $app->urlFor("PROJET2",['id' => 2]);
+        $urlProjet2Question3 = $app->urlFor("PROJET2",['id' => 3]);
+        $urlProjet2Question4 = $app->urlFor("PROJET2",['id' => 4]);
+        $urlProjet2Question5 = $app->urlFor("PROJET2",['id' => 5]);
+        $urlProjet2Question6 = $app->urlFor("PROJET2",['id' => 6]);
+        $urlProjet2Question7 = $app->urlFor("PROJET2",['id' => 7]);
 
             $html = <<<END
-<h3>Projet 1</h3>
-<ul>
-  <li><a href="$urlQuestion1">Question 1</a></li>
-  <li><a href="$urlQuestion2">Question 2</a></li>
-  <li><a href="$urlQuestion3">Question 3</a></li>
-  <li><a href="$urlQuestion4">Question 4</a></li>
-  <li><a href="$urlQuestion5">Question 5</a></li>
-</ul>
-<h3>Projet 2</h3>
-<h3>Projet 3</h3>
+                <h3>Projet 1</h3>
+                <ul>
+                  <li><a href="$urlProjet1Question1">Question 1</a></li>
+                  <li><a href="$urlProjet1Question2">Question 2</a></li>
+                  <li><a href="$urlProjet1Question3">Question 3</a></li>
+                  <li><a href="$urlProjet1Question4">Question 4</a></li>
+                  <li><a href="$urlProjet1Question5">Question 5</a></li>
+                </ul>
+                <h3>Projet 2</h3>
+                <ul>
+                    <li><a href="$urlProjet2Question1">Question 1</a></li>
+                    <li><a href="$urlProjet2Question2">Question 2</a></li>
+                    <li><a href="$urlProjet2Question3">Question 3</a></li>
+                    <li><a href="$urlProjet2Question4">Question 4</a></li>
+                    <li><a href="$urlProjet2Question5">Question 5</a></li>
+                    <li><a href="$urlProjet2Question6">Question 6</a></li>
+                    <li><a href="$urlProjet2Question7">Question 7</a></li>
+                </ul>
 END;
 
         return $html;
     }
 
-    public function htmlquestionP1Q1()
-    {
-        $res = "<h3>Question n°1 : liste des jeux contenant Mario dans leur titre</h3>";
-        foreach($this->elements as $game) {
-            $res = $res."Nom du jeu : $game->name </br>";
-        }
-        return $res;
-    }
-    public function htmlquestionP1Q2()
-    {
-        $res = "<h3>Question n°2 : liste des compagnies installees au japon</h3>";
-        foreach($this->elements as $company) {
-            $res = $res."Nom de la compagnie : $company->name <br>";
-        }
-        return $res;
-    }
-    public function htmlquestionP1Q3()
-    {
-        $res = "<h3>Question n°3 : liste des plateformes ayant plus de 10M d'installations</h3>";
-        foreach($this->elements as $platform) {
-            $res = $res."Nom de la plateforme : $platform->name <br>";
-        }
-        return $res;
-    }
-    public function htmlquestionP1Q4()
-    {
-        $res = "<h3>Question n°4 : liste de 442 jeux à partir du 21173ème</h3>";
-        $res = $res.$this->elements;
-        return $res;
-    }
-    public function htmlquestionP1Q5()
-    {
-        $res = "<h3>Question n°5 : lister les jeux, afficher leur nom et deck, en paginant (taille des pages : 500)</h3>";
-        $res = $res.$this->elements;
-        return $res;
-    }
-
     public function render()
     {
-        switch ($this->selecteur) {
-            case "ALL_VIEW" :
-                {
-                    $content = $this->htmlquestion();
-                    break;
-                }
-            case "P1Q1" :
-                {
-                    $content = $this->htmlquestionP1Q1();
-                    break;
-                }
-            case "P1Q2" :
-                {
-                    $content = $this->htmlquestionP1Q2();
-                    break;
-                }
-            case "P1Q3" :
-                {
-                    $content = $this->htmlquestionP1Q3();
-                    break;
-                }
-            case "P1Q4" :
-                {
-                    $content = $this->htmlquestionP1Q4();
-                    break;
-                }
-            case "P1Q5" :
-                {
-                    $content = $this->htmlquestionP1Q5();
-                    break;
-                }
-        }
-
+        $menu = $this->htmlquestion();
+        $content = $this->elements;
         $html = <<<END
 <!DOCTYPE html>
 <html>
-<head><a href="$this->urlServeur/">accueil</a></head>
+<head><a href="$this->urlServeur">accueil</a></head>
 <body>
- 
+ <div class="menu">
+ $menu
+</div>
 <div>
  $content
 </div>
