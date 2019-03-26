6d54189da5de15db4c197e7a2103aa0480f7a91e
Adrien <adrien.palmieri9@etu.univ-lorraine.fr>
2019-03-26 18:45:57 +0100

    optimisation du code

--- Projet_1/PHP/src/controleurs/ControleurAPI.php
+++ Projet_1/PHP/src/controleurs/ControleurAPI.php
@@ -3,24 +3,27 @@ namespace gamepedia\controleurs;
 use gamepedia\models\Game;
 use gamepedia\vues\VueAPI;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Slim\Slim as Slim;
 
 class ControleurAPI
 {
-
+    private $app;
+    public function __construct()
+    {
+        $this->app = Slim::getInstance();
+    }
 
     public function displayGameJson($id_jeu) {
-        $app = \Slim\Slim::getInstance();
         try{
           $jeu = Game::where('id','=',$id_jeu)->firstOrFail();
         }catch (ModelNotFoundException $e){
-          $app->response->setStatus(404);
-          $app->response->headers->set('Content-Type', 'application/json');
-          echo json_encode(['error' => 404, 'message'=>'game not found']);
+            $this->app->response->setStatus(404);
+            $this->app->response->headers->set('Content-Type', 'application/json');
+          echo json_encode(['error' => 404, 'Information'=>'Jeu demande non trouve']);
           return;
         }
-
-        $app->response->setStatus(200);
-        $app->response->headers->set('Content-Type', 'application/json');
+        $this->app->response->setStatus(200);
+        $this->app->response->headers->set('Content-Type', 'application/json');
         echo json_encode($jeu->toArray());
 
     }
@@ -28,32 +31,27 @@ class ControleurAPI
 
 
     public function displayGameCharacters($id_jeu) {
-        $app = \Slim\Slim::getInstance();
         try{
             $jeu = Game::where('id','=',$id_jeu)->firstOrFail();
             $personnages = $jeu->personnages()->get();
         }catch (ModelNotFoundException $e){
-            $app->response->setStatus(404);
-            $app->response->headers->set('Content-Type', 'application/json');
+            $this->app->response->setStatus(404);
+            $this->app->response->headers->set('Content-Type', 'application/json');
             echo json_encode(['error' => 404, 'Information'=>'Jeu demande non trouve']);
             return;
         }
-        $app->response->setStatus(200);
-        $app->response->headers->set('Content-Type', 'application/json');
+        $this->app->response->setStatus(200);
+        $this->app->response->headers->set('Content-Type', 'application/json');
         echo json_encode($personnages->toArray());
     }
+
+
     public function displayGames() {
-        $app = \Slim\Slim::getInstance();
         $premiersJeux = array();
-
         $paramValue = \Slim\Slim::getInstance()->request()->get('page');
-        if(!isset($paramValue) || !is_numeric($paramValue)) {
-
-            $paramValue = 1;
-
-        }
+        if(!isset($paramValue) || !is_numeric($paramValue))  $paramValue = 1;
         try{
-            $url = $app->request()->getHostWithPort().$app->request()->getRootUri();
+            $url = $this->app->request()->getHostWithPort().$this->app->request()->getRootUri();
             if($paramValue <= 0)    $paramValue = 1;
             for($i = (($paramValue-1)*200) + 1 ; $i <= (200*$paramValue); $i++) {
                 $jeu = Game::where('id','=',$i)->first();
@@ -68,14 +66,13 @@ class ControleurAPI
             $premiersJeux['links'] = array('prec'=>"http://$url/api/games?page=$pagePrec",
                                            'suiv'=>"http://$url/api/games?page=$pageSuiv");
         }catch (ModelNotFoundException $e){
-            $app->response->setStatus(404);
-            $app->response->headers->set('Content-Type', 'application/json');
+            $this->app->response->setStatus(404);
+            $this->app->response->headers->set('Content-Type', 'application/json');
             echo json_encode(['error' => 404, 'Information'=>'Jeu demande non trouve']);
             return;
         }
-        $app->response->setStatus(200);
-        $app->response->headers->set('Content-Type', 'application/json');
-
+        $this->app->response->setStatus(200);
+        $this->app->response->headers->set('Content-Type', 'application/json');
         $json = json_encode($premiersJeux);
         echo $json;
 
