2ed2167967eb049088bcaf2c98b22b22b7d24c5c
Nicolas Kircher <nicolas.kircher5@etu.univ-lorraine.fr>
2019-03-26 16:34:24 +0100

    Ajout du try and catch et du message d'erreur si le jeu n'existe pas

--- Projet_1/PHP/src/controleurs/ControleurAPI.php
+++ Projet_1/PHP/src/controleurs/ControleurAPI.php
@@ -2,26 +2,48 @@
 namespace gamepedia\controleurs;
 use gamepedia\models\Game;
 use gamepedia\vues\VueAPI;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ControleurAPI
 {
 
 
     public function displayGameJson($id_jeu) {
-        $jeu = Game::where('id','=',$id_jeu)->first();
-        if($jeu != null) {
-            (new VueAPI($jeu))->render();
+        $app = \Slim\Slim::getInstance();
+        try{
+          $jeu = Game::where('id','=',$id_jeu)->firstOrFail();
+        }catch (ModelNotFoundException $e){
+          $app->response->setStatus(404);
+          $app->response->headers->set('Content-Type', 'application/json');
+          echo json_encode(['error' => 404, 'message'=>'game not found']);
+          return;
         }
+
+        $app->response->setStatus(200);
+        $app->response->headers->set('Content-Type', 'application/json');
+        echo json_encode($jeu->toArray());
+
     }
 
     public function displayGames() {
+        $app = \Slim\Slim::getInstance();
         $premiersJeux = array();
         for($i = 1 ; $i <= 200 ; $i++) {
+          try{
             $test = Game::where('id','=',$i)->first();
-            $premiersJeux[$i] = $test;
-
+          }catch (ModelNotFoundException $e){
+              $app->response->setStatus(404);
+              $app->response->headers->set('Content-Type', 'application/json');
+              echo json_encode(['error' => 404, 'message'=>'game not found']);
+              return;
+          }
+          $premiersJeux[$i] = $test;
         }
 
-        (new VueAPI($premiersJeux))->render();
+
+        $app->response->setStatus(200);
+        $app->response->headers->set('Content-Type', 'application/json');
+        echo json_encode($premiersJeux);
+
     }
 }
